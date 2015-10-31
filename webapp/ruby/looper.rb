a9e18a3e21d3c47a72e96de3111b227048e67ddf
Kazuki Yamaguchi <k@rhe.jp>
2015-10-31 16:53:13 +0900

    looper

---
+++ webapp/ruby/looper.rb
@@ -0,0 +1,26 @@
+require 'oj'
+require 'httpclient'
+require 'openssl'
+require "redis"
+
+def fetch_api(uri, headers, params)
+  @client ||= HTTPClient.new
+  if uri.start_with? "https://"
+    @client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  end
+  @client.get_content(uri, params, headers)
+end
+
+tenki = Thread.new {
+  loop do
+    _a = fetch_api("http://api.five-final.isucon.net:8988/", {}, {})
+    redis.set("tenki", _a)
+    sleep 1
+  end
+}
+
+attacked = Thread.new {
+}
+
+tenki.join
+attacked.join
