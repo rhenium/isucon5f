b882e9b5f86e437aae2d1b596260843c3ce361d0
Kazuki Yamaguchi <k@rhe.jp>
2015-10-31 15:46:13 +0900

    hash

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -64,7 +64,7 @@ class Isucon5f::WebApp < Sinatra::Base
     nid = redis.incr("user_lid")
     salt = generate_salt
     passhash = '\\x' + Digest::SHA512.hexdigest(salt + params['password'])
-    u = { id: nid.to_i, email: params['email'], grade: params['grade'], passhash: hash, salt: salt }
+    u = { id: nid.to_i, email: params['email'], grade: params['grade'], passhash: passhash, salt: salt }
     redis.hset("users", params['email'], Oj.dump(u))
     redis.hset("subscriptions", nid.to_s, "{}")
     redirect '/login'
