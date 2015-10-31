f2c416d29bf8074d4ee4b09b3ca50474571ba77b
Kazuki Yamaguchi <k@rhe.jp>
2015-10-31 11:19:14 +0900

    sock

--- webapp/ruby/config.ru
+++ webapp/ruby/config.ru
@@ -1,3 +1,4 @@
 require_relative './app.rb'
 
+use Rack::Lineprof
 run Isucon5f::WebApp
--- webapp/ruby/unicorn_config.rb
+++ webapp/ruby/unicorn_config.rb
@@ -1,4 +1,5 @@
 worker_processes 4
 preload_app true
 listen 8080
+listen "/sock/unicorn.sock", backlog: 4096
 # pid "/home/isucon/webapp/ruby/unicorn.pid"
